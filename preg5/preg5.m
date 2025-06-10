b6879d0347d15eb7df673520846e120b85b1618d
luisguillermo900 <lchirinosco@unsa.edu.pe>
2025-06-09 23:57:59 -0500

    actualización

--- preg5/preg5.m
+++ preg5/preg5.m
@@ -1,25 +1,29 @@
-% P a r m e t r o s b s i c o s
-A = 1; B = 1; C = 1;
-a = 3; b = 2; c = 5;
+% Parámetros básicos
+A = 1;
+B = 1;
+C = 1;
+a = 3;
+b = 2;
+c = 5;
 delta_x = 0;
-delta_y = pi /2;
-delta_z = pi /4;
+delta_y = pi / 2;
+delta_z = pi / 4;
 
 % Tiempo
-t = linspace (0 , 2*pi , 1000) ;
+t = linspace(0, 2*pi, 1000);
 
-% Ecuaciones p a r a m t r i c a s
-x = A * sin (a*t + delta_x );
-y = B * sin (b*t + delta_y );
-z = C * sin (c*t + delta_z );
+% Ecuaciones paramétricas
+x = A * sin(a*t + delta_x);
+y = B * sin(b*t + delta_y);
+z = C * sin(c*t + delta_z);
 
-% G r f i c a
-figure ;
-plot3 (x , y , z , 'LineWidth ', 2) ;
-grid on ;
-xlabel ('X');
-ylabel ('Y');
-zlabel ('Z');
-title ('Figura de Lissajous en 3D');
-axis equal ;
-view (45 , 30) ;
+% Gráfica
+figure;
+plot3(x, y, z, 'LineWidth', 2);
+grid on;
+xlabel('X');
+ylabel('Y');
+zlabel('Z');
+title('Figura de Lissajous en 3D');
+axis equal;
+view(45, 30);
--- preg5/preg51.m
+++ preg5/preg51.m
@@ -1,39 +1,42 @@
-% F u n c i n para generar figuras de Lissajous 3D
-function [x , y , z] = lissajous_3d (t , Ax , Ay , Az , wx , wy , wz , phix , phiy , phiz )
-x = Ax * sin ( wx * t + phix );
-y = Ay * sin ( wy * t + phiy );
-z = Az * sin ( wz * t + phiz );
+% Función para generar figuras de Lissajous 3D
+function [x, y, z] = lissajous_3d(t, Ax, Ay, Az, wx, wy, wz, phix, phiy, phiz)
+    x = Ax * sin(wx * t + phix);
+    y = Ay * sin(wy * t + phiy);
+    z = Az * sin(wz * t + phiz);
 end
 
-% C o n f i g u r a c i o n e s diferentes
+% Configuraciones diferentes
 configuraciones = {
-% [Ax , Ay , Az , wx , wy , wz , phix , phiy , phiz ]
-[1 , 1, 1 , 1 , 2 , 3 , 0, pi /2 , pi /4] , % C o n f i g u r a c i n 1
-[1 , 1, 1 , 3 , 2 , 5 , 0, pi /2 , pi /4] , % C o n f i g u r a c i n 2
-[1 , 1, 1 , 2 , 3 , 1 , 0, 0, pi /2] , % C o n f i g u r a c i n 3
-[1 , 1, 1 , 5 , 4 , 3 , pi /4 , pi /2 , 0] % C o n f i g u r a c i n 4
+    % [Ax, Ay, Az, wx, wy, wz, phix, phiy, phiz]
+    [1, 1, 1, 1, 2, 3, 0,      pi/2, pi/4],   % Configuración 1
+    [1, 1, 1, 3, 2, 5, 0,      pi/2, pi/4],   % Configuración 2
+    [1, 1, 1, 2, 3, 1, 0,      0,    pi/2],   % Configuración 3
+    [1, 1, 1, 5, 4, 3, pi/4,   pi/2, 0   ]    % Configuración 4
 };
 
-% P a r m e t r o temporal
-t = linspace (0 , 4*pi , 2000) ;
+% Parámetro temporal
+t = linspace(0, 4*pi, 2000);
 
-% Generar m l t i p l e s figuras
-figure ('Position ', [100 , 100 , 1200 , 800]) ;
+% Generar múltiples figuras
+figure('Position', [100, 100, 1200, 800]);
 
-for i = 1: length ( configuraciones )
-config = configuraciones {i };
-[x , y , z] = lissajous_3d (t , config (1) , config (2) , config (3) , ...
-config (4) , config (5) , config (6) , ...
-config (7) , config (8) , config (9) ) ;
+for i = 1:length(configuraciones)
+    config = configuraciones{i};
+    
+    [x, y, z] = lissajous_3d(t, config(1), config(2), config(3), ...
+                                config(4), config(5), config(6), ...
+                                config(7), config(8), config(9));
 
-subplot (2 , 2, i);
-plot3 (x , y , z , 'LineWidth ', 1.5) ;
-grid on ;
-xlabel ('X'); ylabel ('Y'); zlabel ('Z') ;
-title ( sprintf (' C o n f i g u r a c i n %d: = [ %g, %g, %g]', i , ...
-config (4) , config (5) , config (6) )) ;
-axis equal ;
-view (45 , 30) ;
+    subplot(2, 2, i);
+    plot3(x, y, z, 'LineWidth', 1.5);
+    grid on;
+    xlabel('X');
+    ylabel('Y');
+    zlabel('Z');
+    title(sprintf('Configuración %d: [%.0f, %.0f, %.0f]', ...
+          i, config(4), config(5), config(6)));
+    axis equal;
+    view(45, 30);
 end
 
-sgtitle ('Figuras de Lissajous 3D con diferentes configuraciones ');
+sgtitle('Figuras de Lissajous 3D con diferentes configuraciones');
